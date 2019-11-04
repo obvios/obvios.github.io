eac6fa0794d1388fdeea2d52c26fa091c99862de
Eric Palma <palmae95@gmail.com>
2019-11-04 12:14:39 -0800

    removed kali modal

--- websiteJS.js
+++ websiteJS.js
@@ -1,21 +1,4 @@
-﻿//Modal for Kali Linux
-var modal0 = document.getElementById('kaliLinuxModal');
-
-var img0 = document.getElementById('KaliLinuxIMG');
-var modalImg0 = document.getElementById("IMG00");
-img0.onclick = function() {
-    modal0.style.display = "block";
-    modalImg0.src = this.src;
-}
-
-var span0 = document.getElementsByClassName("close")[0];
-
-span0.onclick = function() {
-    modal0.style.display = "none";
-}
-
-
-//Modal for C++
+﻿//Modal for C++
 var modal1 = document.getElementById('c++Modal');
 var img1 = document.getElementById('CIMG');
 var modalImg1 = document.getElementById("IMG01");
