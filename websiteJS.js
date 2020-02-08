ff98364892498d3fb6c28b1e320f2d11b6948176
Eric Palma <palmae95@gmail.com>
2020-02-07 19:50:09 -0800

    refactoring

--- websiteJS.js
+++ websiteJS.js
@@ -1,22 +1,27 @@
-﻿//updated script//
+'use strict';
+
+//set up lists with classes
 var modals = document.getElementsByClassName("modal");
 var images = document.getElementsByClassName("skillsIMG");
 var modalImages = document.getElementsByClassName("modal-content");
 var spans = document.getElementsByClassName("close");
 
-var createHandler = function (index,modalList, modalImgs){
-    return function(){
+//functions to create handler for onclick events of images
+function createHandler(index, modalList, modalImgs){
+    return function() {
         modalList[index].style.display = "block";
         modalImgs[index].src = this.src;
     }
 }
 
-var createSpanHandler = function(index, modalList){
+//functions to create handler for onclick events of close button in modal
+function createSpanHandler(index, modalList){
     return function() {
         modalList[index].style.display = "none";
     }
 }
 
+//initialize all image click handlers and modal span buttons
 for (var i = 0; i < modals.length; i++) {
     images[i].onclick = createHandler(i, modals, modalImages);
     spans[i].onclick = createSpanHandler(i, modals);
